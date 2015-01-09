051113f12998e166fa6631543189c8f8e512c480
Hugo W <hugo.wnt@gmail.com>
2015-01-09 10:50:30 -0200

    Fix bug related to single line declarations on postgres

--- vagrant/postgres.sh
+++ vagrant/postgres.sh
@@ -7,7 +7,10 @@ sudo -u postgres psql -c "CREATE USER root SUPERUSER INHERIT createdb createrole
 sudo -u postgres psql -c "ALTER USER root PASSWORD 'root';"
 sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO root;"
 sudo -u postgres psql -c "ALTER USER postgres PASSWORD 'postgres';"
-sudo -u postgres psql -c "UPDATE pg_database SET datistemplate=false WHERE datname='template1'; DROP DATABASE Template1; CREATE DATABASE template1 WITH owner=postgres encoding='UTF-8' lc_collate='en_US.utf8' lc_ctype='en_US.utf8' template template0; UPDATE pg_database SET datistemplate=true WHERE datname='template1';"
-/etc/init.d/postgresql restart
+sudo -u postgres psql -c "UPDATE pg_database SET datistemplate=false WHERE datname='template1';"
+sudo -u postgres psql -c "DROP DATABASE Template1;"
+sudo -u postgres psql -c "CREATE DATABASE template1 WITH owner=postgres encoding='UTF-8' lc_collate='en_US.utf8' lc_ctype='en_US.utf8' template template0;"
+sudo -u postgres psql -c "UPDATE pg_database SET datistemplate=true WHERE datname='template1';";
+sudo -u postgres /etc/init.d/postgresql restart
 
 echo '...Done.'
